08976980eb8bff0b5d23108392b3e26e6d8cd727
netcon <netcon@live.com>
2023-01-30 21:00:00 +0800

    feat: add remote port

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,17 @@ import Koa from 'koa';
 import { getLocationInfoWithDNS } from './geoip';
 
 const app = new Koa();
+const first = (items: any[] | any) => Array.isArray(items) ? items[0] : items;
 
 app.use(({ request, response }) => {
-  const query = request.path.slice(1) || request.ip;
+  const pathQuery = request.path.slice(1);
+  const query = pathQuery || first(request.headers['x-real-ip']) || request.ip;
 
   return getLocationInfoWithDNS(query).then((locationInfo) => {
-    response.body = locationInfo;
+    response.body = !pathQuery && request.socket ? {
+	...locationInfo,
+        port: first(request.headers['x-real-port']) || request.socket.remotePort,
+    } : locationInfo;
   }).catch((error) => {
     response.status = 400;
     response.body = {
